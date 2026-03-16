07c7689731e3e91f08db310c0078de6d7cfd61c5
Danniel Céspedes Velásquez <danielcorecspds@gmail.com>
2026-03-16 04:22:22 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4236454254-aba86",
+  "appId": "1:797121337628:web:d93329d774a8ed4e7402d9",
+  "apiKey": "AIzaSyCbu3LRbOnM2WJtKCAEg1nuFIuOtDXvY5Q",
+  "authDomain": "studio-4236454254-aba86.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "797121337628"
+};
